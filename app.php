f19f67c199c7de35588759ddc1c8f6ec7c27cb4d
Eliu Gonzalez Hernandez <elgoher@gmail.com>
2014-01-18 10:52:03 -0500

    version inicial de enkonduki

---
+++ app.php
@@ -0,0 +1,16 @@
+<?php
+echo "aqui toy";
+//metodo donde esta recibe el header, el footer y el target del archivo a cambiar
+function insertar($headerUrl,$footerUrl,$targetUrl)
+{
+	$header = file_get_contents($headerUrl); //cargo el archivo header.html
+	$footer = file_get_contents($footerUrl); //cargo el archivo footer.html
+	$target = file_get_contents($targetUrl); //cargo el archivo target.html
+
+	$target = str_ireplace('<header>', $header, $target); // se inserta el header en target.html
+	$target = str_ireplace("<footer>", $footer, $target); // se inserta el footer en target.html
+
+	file_put_contents($url,$target);//se guarda el archivo modificado
+	header("Location: $index.php");//se redirecciona a la url
+}
+?>
